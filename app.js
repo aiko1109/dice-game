84625cee671ac690fe5914315408fc514f948c11
aiko <bakinaa13@gmail.com>
2022-07-08 11:44:32 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+var activePlayer = 1;
 var scores = [0, 0];
 var roundScore = 0;
-var dice = Math.floor(Math.random()* 6) + 1;
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
-console.log("Шоо :" + dice);
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.scr = "dice-" + diceNumber + ".png";
+});
