5d0d87c2edf149afc9526bc4af665139e6da005e
aiko <bakinaa13@gmail.com>
2022-07-08 12:35:16 +0800

    toglogchiin eeljiig solih

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-var activePlayer = 1;
+var activePlayer = 0;
 var scores = [0, 0];
 var roundScore = 0;
 document.getElementById("score-0").textContent = "0";
 document.getElementById("score-1").textContent = "0";
 document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
-
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 document.querySelector(".btn-roll").addEventListener("click", function() {
-    var diceNumber = Math.floor(Math.random() * 6) + 1;
-    diceDom.style.display = "block";
-    diceDom.scr = "dice-" + diceNumber + ".png";
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+  if (diceNumber !== 1) {
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    // var obj = {
+    //   name: "amaraa",
+    //   job: "programmer",
+    //   address: {
+    //     city: "ub",
+    //     country: "mn"
+    //   }
+    // };
+    // obj.address.country;
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    diceDom.style.display = "none";
+    // if (activePlayer === 0) {
+    //   activePlayer = 1;
+    // } else {
+    //   activePlayer = 0;
+    // }
+  }
 });
